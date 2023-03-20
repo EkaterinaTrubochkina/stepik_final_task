8860cdc384aefe5fc0e7118b035cbea215597dd1
DESKTOP-2T9NM3V\Администратор <trubochkina.ekaterina@yandex.ru>
2023-03-20 22:00:58 +0300

    Task 4.2.8. LoginPage:added;Locators:added for LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -2,9 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
@@ -14,3 +15,9 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+    
+def test_login_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        login_page.should_be_login_page()
